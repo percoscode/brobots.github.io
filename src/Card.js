3ae247a78180660365fe2d5a01bc7f94cb2907c4
Will <percoschat@gmail.com>
2019-06-20 05:38:14 -0700

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        <div>
+            <img src='' alt='brobot'/>
+            <div>
+                <h2>Chad Muska</h2>
+                <p>themuska@muska.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
