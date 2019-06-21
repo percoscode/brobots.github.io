5d4bbb85ad8376a56280f9c83e446f45411c9a2e
Will <percoschat@gmail.com>
2019-06-21 06:27:35 -0700

    added search and title

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'; 
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+    render() {
+        return (
+            <div className='tc'>
+                <h1>Brobots!</h1>
+                <SearchBox />
+                <CardList robots={this.state.robots}/>
+            </div>
+        )
+    }
+}
+
+// export default class extends react.Component
+export default App 
